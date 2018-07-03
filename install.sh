309d14687c492faaaac9b796efe4ba6f44de4d3f
aha-oretama <sekine_y_529@msn.com>
2018-07-03 16:03:17 +0900

    Bugfix, need to login Apple for mas-cli.

--- install.sh
+++ install.sh
@@ -1,16 +1,39 @@
+#!/bin/bash
+
+
 # homebrew [https://brew.sh/]
 ## Notification: required for OS X Lion 10.7 and below.
 if [[ ! "$(which brew && brew help)" ]]; then
+  echo 'install homebrew'
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+
 # mas-cli [https://github.com/mas-cli/mas]
-if [[ ! "$(brew list | grep mas)" ]]; then 
+if [[ ! "$(brew list | grep mas)" ]]; then
+  echo 'install mas-cli'
   brew install mas
 fi
 mas outdated
 
+## change cli from gui after fix https://github.com/mas-cli/mas/issues/107
+# echo "input your Apple ID"
+# read itunes_user
+# echo "${itunes_user}"
+# mas signin "${itunes_user}"
+echo -e "\nPlease log in to the app store..."
+
+open -a "/Applications/App Store.app"
+
+until (mas account > /dev/null);
+do
+  sleep 3
+done
+
+
 # The Unarchiver [https://itunes.apple.com/jp/app/the-unarchiver/id425424353]
-if [[ ! "$(mas list | grep 425424353)" ]; then
+if [[ ! "$(mas list | grep 425424353)" ]]; then
+  echo 'install The Unarchiver'
   mas install 425424353
 fi
+
