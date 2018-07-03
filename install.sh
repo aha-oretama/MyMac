53c5f23822e30c6e95bb2f8694f552843566d1cb
aha-oretama <sekine_y_529@msn.com>
2018-07-03 17:59:16 +0900

    refactor: Refactoring brew install.

--- install.sh
+++ install.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
+## homebrew [https://brew.sh/]
+### Notification: required for OS X Lion 10.7 and below.
+#if [[ ! "$(which brew && brew help)" ]]; then
+#  echo 'install homebrew'
+#  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#fi
 
-# homebrew [https://brew.sh/]
-## Notification: required for OS X Lion 10.7 and below.
-if [[ ! "$(which brew && brew help)" ]]; then
-  echo 'install homebrew'
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
+# brew install
+function brew_install() {
+  curl -fsSL https://raw.githubusercontent.com/aha-oretama/MyMac/master/brewlist.csv | while read item
+  do
+    if [[ ! "$(brew list | grep ${item})" ]]; then
+      echo "install ${item}"
+      brew install "${item}"
+    fi
+  done
+}
+brew_install
 
 # mas-cli [https://github.com/mas-cli/mas]
-if [[ ! "$(brew list | grep mas)" ]]; then
-  echo 'install mas-cli'
-  brew install mas
-fi
-mas outdated
-
 ## change cli from gui after fix https://github.com/mas-cli/mas/issues/107
 # echo "input your Apple ID"
 # read itunes_user
 # echo "${itunes_user}"
 # mas signin "${itunes_user}"
-echo -e "\nPlease log in to the app store..."
+if [[ ! "$(mas account)" ]]; then
+  echo -e "\nPlease log in to the app store..."
+  open -a "/Applications/App Store.app"
 
-open -a "/Applications/App Store.app"
-
-until (mas account > /dev/null);
-do
-  sleep 3
-done
+  until (mas account > /dev/null);
+  do
+    sleep 3
+  done
+fi
 
 
 # The Unarchiver [https://itunes.apple.com/jp/app/the-unarchiver/id425424353]
@@ -46,14 +51,14 @@ if [[ ! "$(which nodebrew && nodebrew help)" ]]; then
 fi
 
 # node [https://nodejs.org/ja/]
-if [[ ! "$(which node && node --version) ]]; then
-  local node_version="$(nodebrew ls-all | grep -E "^v" | tail -n 1)"
+if [[ ! "$(which node && node --version)" ]]; then
+  node_version="$(nodebrew ls-all | grep -E "^v" | tail -n 1)"
   echo "install node, version is ${node_version}"
   nodebrew install "${node_version}"
 fi
 
 # commitizen [https://github.com/commitizen/cz-cli]
-if [[ ! "$(which cz)" ]]; then
+if [[ ! "$(npm ls -g --depth=0 | grep commitizen)" ]]; then
   echo 'install commitizen'
   npm install -g commitizen cz-conventional-changelog
   echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
