8ddbbf6913f5e33b5d1d91c2b7501196fb0f81c0
aha-oretama <sekine_y_529@msn.com>
2019-11-08 21:00:00 +0900

    add mac-setup

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # MyMac
 Set up my mac.
-It is highly recommend to use [dotfiles](https://github.com/aha-oretama/dotfiles)'s `install.sh` after using this `install.sh`.
+It is highly recommend to use [dotfiles](https://github.com/aha-oretama/dotfiles)'s `install.sh` after using this `mac-setup.sh` and `install.sh`.
 
 ## How to use.
 
 ```
-curl -H 'Cache-Control: no-cache' -fsSL 'https://raw.githubusercontent.com/aha-oretama/MyMac/master/install.sh' | bash
+$ curl -H 'Cache-Control: no-cache' -fsSL 'https://raw.githubusercontent.com/aha-oretama/MyMac/master/mac-setup.sh' | bash
+$ curl -H 'Cache-Control: no-cache' -fsSL 'https://raw.githubusercontent.com/aha-oretama/MyMac/master/install.sh' | bash
 ```
---
+++ mac-setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+defaults write com.apple.touchbar.agent PresentationModeGlobal functionKeys
+## Enable Full key board access
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool false
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+## Enable detail save panel when saving a download file
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -boolean true
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+
+# Trackpad
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.menuextra.clock DateFormat -string "M\u6708d\u65e5(EEE)  H:mm:ss"
+
+## Open spotlight by ctl + space
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "
+<dict>
+  <key>enabled</key>
+  <true/>
+  <key>value</key>
+  <dict>
+    <key>parameters</key>
+    <array>
+      <integer>65535</integer>
+      <integer>49</integer>
+      <integer>524288</integer>
+    </array>
+    <key>type</key>
+    <string>standard</string>
+  </dict>
+</dict>"
+## Change input language by command + space
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 60 "
+<dict>
+  <key>enabled</key>
+  <true/>
+  <key>value</key>
+  <dict>
+    <key>parameters</key>
+    <array>
+      <integer>65535</integer>
+      <integer>49</integer>
+      <integer>1048576</integer>
+    </array>
+    <key>type</key>
+    <string>standard</string>
+  </dict>
+</dict>"
+
+
+# Dock
+defaults write com.apple.dock autohide -bool false
+defaults write com.apple.dock orientation -string "left"
+killall Dock
+
+# Finder
+defaults write com.apple.finder AppleShowAllFiles -bool true
+## Show full path
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+chflags nohidden ~/Library
+sudo chflags nohidden /Volumes
+killall Finder
+
+# Screen Capture
+mkdir -p ~/Desktop/screenCapture
+defaults write com.apple.screencapture location ~/Desktop/screenCapture
+killall SystemUIServer
+
+
